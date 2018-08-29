a5155333324e1f57af513e536d42eedb93011f3e
gongsiwin10 <lhuibin@gmail.com>
2018-08-29 14:18:10 +0800

    '增加百度图片爬取'

--- SpiderImage/imgbaidu.py
+++ SpiderImage/imgbaidu.py
@@ -1,58 +1,72 @@
 # coding=utf-8
 """根据搜索词下载百度图片
-	教程来自于：https://blog.csdn.net/qq_37389133/article/details/79327948  """
+	教程来自于：https://blog.csdn.net/qq_34209701/article/details/72638966  """ # 问题是遇到不能下载的图片就停止了。
+
+
 import requests
-from urllib.request import urlretrieve
 import os
+ 
+def getPages(keyword,pages):
+    params = []
+    for i in range(30, 30*pages+30, 30):
+        params.append({
+                      'tn': 'resultjson_com',
+                      'ipn': 'rj',
+                      'ct': 201326592,
+                      'is': '',
+                      'fp': 'result',
+                      'queryWord': keyword,
+                      'cl': 2,
+                      'lm': -1,
+                      'ie': 'utf-8',
+                      'oe': 'utf-8',
+                      'adpicid': '',
+                      'st': -1,
+                      'z': '',
+                      'ic': 0,
+                      'word': keyword,
+                      's': '',
+                      'se': '',
+                      'tab': '',
+                      'width': '',
+                      'height': '',
+                      'face': 0,
+                      'istype': 2,
+                      'qc': '',
+                      'nc': 1,
+                      'fr': '',
+                      'pn': i,
+                      'rn': 30,
+                      'gsm': '1e',
+                      '1488942260214': ''
+                  })
+    url = 'https://image.baidu.com/search/acjson'
+    urls = []
+    for i in params:
+        urls.append(requests.get(url, params=i).json().get('data'))
+    return urls
+ 
+def getImg(dataList, localPath):
+ 
+    if not os.path.exists(localPath):
+        os.mkdir(localPath)
+ 
+    try:
+        x = 0
+        for list in dataList:
+            for i in list:
+                if i.get('thumbURL') != None:
+                    print('正在下载：%s' % i.get('thumbURL'))
+                    ir = requests.get(i.get('thumbURL'))
+                    open(localPath + '%d.jpg' % x, 'wb').write(ir.content)
+                    x += 1
+        print('图片下载完成')
+    except Exception:
+        print("图片下载失败")
+ 
+if __name__ == '__main__':
+    dataList = getPages('汽修店', 3)
+    getImg(dataList, 'D:/images/')
 
-def Search(name,localpath,page):
-    os.makedirs(localpath, exist_ok=True)  #这里创建文件夹路径，exist_ok=True 指如果有就不创建
-    params = {
-        'tn':'resultjson_com',
-        'catename':'pcindexhot',
-        'ipn':'rj',
-        'ct':'201326592',
-        'is':'',
-        'fp':'result',
-        'queryWord':'',
-        'cl':'2',
-        'lm':'-1',
-        'ie':'utf-8',
-        'oe':'utf-8',
-        'adpicid':'',
-        'st':'-1',
-        'z':'',
-        'ic':'0',
-        'word':name,
-        'face':'0',
-        'istype':'2',
-        'qc':'',
-        'nc':'1',
-        'fr':'',
-        'pn':'0',
-        'rn':'30'
-        };
-    params['pn'] = '%d' % page
-    Request(params,localpath)
-    return
 
-def Request(param,path):
-    searchurl  = 'http://image.baidu.com/search/avatarjson'   #百度图片
-    response = requests.get(searchurl,params =param )         #传入请求参数
-    json  = response.json()['imgs']    #每张图片有自己的下载路径
-    for i in range(0,len(json)):
-        filename = os.path.split(json[i]['objURL'])[1]
-        Download(json[i]['objURL'],filename,path)
-def Download(url,filename,filepath):
-    path = os.path.join(filepath,filename)      #这里我们还是采用原来的图片
-    try:                                                 #有些图片不知道为什么下载不了，所以这里用了try的方式
-        urlretrieve(url,path)
-        print('Downloading Images From ', url)
-    except:
-        print('Downloading None Images!')
-
-#下载的主函数
-if __name__ =='__main__':
-    for i in range(1,2):
-      Search('汽修店','data/bibi',i)    #这里可以方便的下载指定的页数
 
---
+++ SpiderImage/imgbaidu2.py
@@ -0,0 +1,61 @@
+# coding=utf-8
+"""根据搜索词下载百度图片
+	教程来自于：https://blog.csdn.net/qq_37389133/article/details/79327948  """# 问题只会下载一页，出在页码列表不对，每次加载30张，页码应该是30,60,90网上加
+
+
+
+import requests
+from urllib.request import urlretrieve
+import os
+
+def Search(name,localpath,page):
+    os.makedirs(localpath, exist_ok=True)  #这里创建文件夹路径，exist_ok=True 指如果有就不创建
+    params = {
+        'tn':'resultjson_com',
+        'catename':'pcindexhot',
+        'ipn':'rj',
+        'ct':'201326592',
+        'is':'',
+        'fp':'result',
+        'queryWord':'',
+        'cl':'2',
+        'lm':'-1',
+        'ie':'utf-8',
+        'oe':'utf-8',
+        'adpicid':'',
+        'st':'-1',
+        'z':'',
+        'ic':'0',
+        'word':name,
+        'face':'0',
+        'istype':'2',
+        'qc':'',
+        'nc':'1',
+        'fr':'',
+        'pn':'0',
+        'rn':'30'
+        };
+    params['pn'] = '%d' % page
+    Request(params,localpath)
+    return
+
+def Request(param,path):
+    searchurl  = 'http://image.baidu.com/search/avatarjson'   #百度图片
+    response = requests.get(searchurl,params =param )         #传入请求参数
+    json  = response.json()['imgs']    #每张图片有自己的下载路径
+    for i in range(0,len(json)):
+        filename = os.path.split(json[i]['objURL'])[1]
+        Download(json[i]['objURL'],filename,path)
+def Download(url,filename,filepath):
+    path = os.path.join(filepath,filename)      #这里我们还是采用原来的图片
+    try:                                                 #有些图片不知道为什么下载不了，所以这里用了try的方式
+        urlretrieve(url,path)
+        print('Downloading Images From ', url)
+    except:
+        print('Downloading None Images!')
+
+#下载的主函数
+if __name__ =='__main__':
+    for i in range(30,120,30): #这里可以方便的下载的图片数，每次json返回30张
+      Search('汽修店','data/bibi',i)    
+
