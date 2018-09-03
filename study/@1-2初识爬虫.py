4e763427ae6fca968dca40b0541ed8e0f82cbb15
homewin10 <lhuibin@gmail.com>
2018-09-03 19:19:45 +0800

    ''

--- study/@1-2初识爬虫.py
+++ study/@1-2初识爬虫.py
@@ -1,7 +1,10 @@
-import requests,io,sys
-r = requests.get('https://book.douban.com/subject/1084336/comments/').text
+import requests,io,sys,pandas
+from bs4 import BeautifulSoup
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030') #改变标准输出的默认编码
+
+'''
+r = requests.get('https://book.douban.com/subject/1084336/comments/').text
 
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(r,'lxml')
@@ -15,3 +18,26 @@ for item in pattern:
 	comments.append(item.text)
 df = pandas.DataFrame(comments)
 df.to_csv('comments.csv')
+'''
+
+#定义函数
+def getHTMLText(url):
+	try:
+		r = requests.get(url,timeout=20) #设置超时时间
+		r.raise_for_status()
+		r.encoding = 'utf8'
+		return r.text
+	except: #异常处理
+		return '产生异常'
+
+if __name__ == '__main__':
+	url = 'https://book.douban.com/subject/1084336/comments/'
+	
+	soup = BeautifulSoup(getHTMLText(url),'lxml') #调用函数
+	pattern = soup.find_all('p','comment-content')
+
+	comments = []
+	for item in pattern:
+		comments.append(item.text)
+	df = pandas.DataFrame(comments)
+	df.to_csv('comments.csv')	
